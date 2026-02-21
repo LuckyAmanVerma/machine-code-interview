17744cf964c16905906234b330927ec806dfb14b
aman-verma <luckyamanverma1@gmail.com>
2026-02-21 10:23:32 +0530

    Rate Limit Fronted

--- src/features/Navigation/constant/index.ts
+++ src/features/Navigation/constant/index.ts
@@ -2,17 +2,13 @@ import React from 'react';
 const sections:string[] = [
     'About Me',
     'Working Experience',
-    'Frontend Architecture',
-    'Fun Activities',
-    'Personal',
+    'Projects'
   ];
 
   const keySections = {
     'about-me': 'About Me',
     'working-experience': 'Working Experience',
-    'frontend-architecture': 'Frontend Architecture',
-    'fun-activities': 'Fun Activities',
-    'personal': 'Personal',
+    'projects': 'Projects',
   };
 
   export {sections, keySections};
